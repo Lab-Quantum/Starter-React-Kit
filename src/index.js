25fd113e637074ea7d2ac62dc12aa51143cbf379
lpeil <luan@peil.dev>
2019-12-05 08:22:56 -0300

    🔲 Change normalize.css

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 import './App.css';
+import 'normalize.css/normalize.css';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
